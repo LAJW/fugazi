dc5c6f490d827a41d8adcaf917eaa2bcfaaa7f69
Łukasz A.J. Wrona <l.wrona@imagho.pl>
2016-04-07 23:11:53 +0200

    TEST: Cover F.param

--- test/main.js
+++ test/main.js
@@ -185,12 +185,19 @@ describe("args", () => {
 })
 
 describe("param", () => {
+
+  it("return undefined if base object is undefined", () => {
+    assert.strictEqual(F.param("one")(undefined), undefined)
+  })
+
   it("get argument from object by key", () => {
     assert.strictEqual(F.param("one", { one : 1 }), 1)
   })
+
   it("get argument from object by numeric key", () => {
     assert.strictEqual(F.param(3, [ "one", "two", "three", "four" ]), "four")
   })
+
 })
 
 describe("ifElse", () => {
