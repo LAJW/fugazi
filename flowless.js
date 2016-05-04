1065285916c99381dc8b97c2b8e3dd88bffae07f
Łukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2016-05-04 22:28:46 +0200

    FEAT: Implement map over stream

--- flowless.js
+++ flowless.js
@@ -267,6 +267,31 @@ const map = {
   enumerable : createMap(generic.enumerable),
   set        : createMap(generic.set),
   map        : createMap(generic.map),
+  stream     : (proc, stream) => {
+    const out = generic.stream.create()
+    let promise
+    stream.on("data", chunk => {
+      const result = proc(chunk)
+      if (promise) {
+        promise = promise
+        .then(() => result)
+        .then(result => out.write(result))
+      } else if (isPromise(result)) {
+        promise = result
+        .then(result => out.write(result))
+      } else if (result) {
+        out.write(result)
+      }
+    })
+    stream.on("end", () => {
+      if (promise) {
+        promise.then(() => out.end())
+      } else {
+        out.end()
+      }
+    })
+    return out
+  }
 }
 
 
