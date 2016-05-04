5ffc303d3045a0fc07ed1095de6448fe19a4037a
Łukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2016-05-04 21:52:19 +0200

    TEST: Add test for asynchornous filter

--- test/main.js
+++ test/main.js
@@ -433,6 +433,16 @@ describe("filter", () => {
     .then(result => assert.deepEqual(result, [ "2", "4", "6" ]))
     .then(() => done(), done)
   })
+  it("filter async callback", done => {
+    Promise.resolve(streamArray([ "1", "2", "3", "4", "5", "6" ]))
+    .then(F.filter(x => Promise.resolve((parseInt(x.toString()) + 1) % 2)))
+    .then(F.reduce((arr, x) => {
+      arr.push(x.toString())
+      return arr
+    }, [ ]))
+    .then(result => assert.deepEqual(result, [ "2", "4", "6" ]))
+    .then(() => done(), done)
+  })
 })
 
 describe("map", () => {
