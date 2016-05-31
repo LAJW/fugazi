dd8dd27ff919016787bfce4db2f4d21b58c01934
Łukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2016-05-31 21:45:23 +0200

    TEST: Add tests for merge

--- test/main.js
+++ test/main.js
@@ -1339,3 +1339,55 @@ describe("assoc", () => {
     .then(() => done(), done)
   })
 })
+
+describe("merge", () => {
+  it("merge two objects", () => {
+    const result = F.merge({ one : "1" }, { two : "2" })
+    assert.deepEqual(result, {
+      one : "1",
+      two : "2"
+    })
+  })
+  it("merge two objects, curried", () => {
+    const result = F.merge({ one : "1" })({ two : "2" })
+    assert.deepEqual(result, {
+      one : "1",
+      two : "2"
+    })
+  })
+  it("merge two promises resolving to objects", done => {
+    F.merge(
+      Promise.resolve({ one : "1" }),
+      Promise.resolve({ two : "2" })
+    )
+    .then(result => {
+      assert.deepEqual(result, {
+        one : "1",
+        two : "2"
+      })
+    })
+    .then(() => done())
+    .catch(done)
+  })
+  it("merge object with function result", () => {
+    const result = F.merge(() => ({ one : "1" }), { two : "2" })
+    assert.deepEqual(result, {
+      one : "1",
+      two : "2"
+    })
+  })
+  it("merge object with asynchronous function result", done => {
+    F.merge(
+      () => Promise.resolve({ one : "1" }),
+      { two : "2" }
+    )
+    .then(result => {
+      assert.deepEqual(result, {
+        one : "1",
+        two : "2"
+      })
+    })
+    .then(() => done())
+    .catch(done)
+  })
+})
